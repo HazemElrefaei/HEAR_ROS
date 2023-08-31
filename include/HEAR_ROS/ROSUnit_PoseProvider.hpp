c6f94b9955da8b3dd9eedc5abac8e1daabc0e258
HazemElrefaei <helrifaei@gmail.com>
2023-08-31 12:21:49 +0100

    Add PX4 vel and ori

--- include/HEAR_ROS/ROSUnit_PoseProvider.hpp
+++ include/HEAR_ROS/ROSUnit_PoseProvider.hpp
@@ -5,11 +5,13 @@
 
 #include "ros/ros.h"
 #ifdef PX4
+    #include <mavros_msgs/VehicleAttitude.h>
     #include <mavros_msgs/VehicleAngularVelocity.h>
 #endif
 #include <geometry_msgs/Vector3Stamped.h>
 #include <geometry_msgs/QuaternionStamped.h>
 #include <geometry_msgs/PoseStamped.h>
+#include <geometry_msgs/TwistStamped.h>
 #include <geometry_msgs/PoseWithCovarianceStamped.h>
 #include <tf2/LinearMath/Matrix3x3.h>
 #include <tf2/LinearMath/Quaternion.h>
@@ -26,12 +28,15 @@ namespace HEAR{
 class ROSUnit_PoseProvider{
 private:
     ros::NodeHandle nh_;
-    ros::Subscriber opti_sub, vision_sub, px4_ang_vel_sub, xsens_ori_sub, xsens_ang_vel_sub, xsens_free_acc_sub;
+    ros::Subscriber opti_sub, vision_sub, px4_pos_sub, px4_vel_sub, px4_ori_sub, px4_ang_vel_sub, xsens_ori_sub, xsens_ang_vel_sub, xsens_free_acc_sub;
     ros::ServiceServer m_server;
     
     ExternalOutputPort<Vector3D<float>>* opti_pos_port;
     ExternalOutputPort<Vector3D<float>>* opti_vel_port;
     ExternalOutputPort<Vector3D<float>>* opti_ori_port;
+    ExternalOutputPort<Vector3D<float>>* px4_pos_port;
+    ExternalOutputPort<Vector3D<float>>* px4_vel_port;
+    ExternalOutputPort<Vector3D<float>>* px4_ori_port;
     ExternalOutputPort<Vector3D<float>>* vision_pos_port;
     ExternalOutputPort<Vector3D<float>>* vision_vel_port;
     ExternalOutputPort<Vector3D<float>>* vision_ori_port;
@@ -41,15 +46,17 @@ private:
     ExternalOutputPort<Vector3D<float>>* px4_imu_angular_rt_port;
     void callback_opti_pose(const geometry_msgs::PoseStamped::ConstPtr& );
     void callback_vision_pose(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr& );
-    void callback_px4_pose(const geometry_msgs::PoseStamped::ConstPtr& );
     void callback_ori(const geometry_msgs::QuaternionStamped::ConstPtr& );
     void callback_free_acc(const geometry_msgs::Vector3Stamped::ConstPtr& );
     void callback_angular_vel(const geometry_msgs::Vector3Stamped::ConstPtr&);
     #ifdef PX4
+    void callback_px4_pose(const geometry_msgs::PoseStamped::ConstPtr& );
+    void callback_px4_vel(const geometry_msgs::TwistStamped::ConstPtr& );
+    void callback_px4_ori(const mavros_msgs::VehicleAttitude::ConstPtr& );
     void callback_px4_angular_vel(const mavros_msgs::VehicleAngularVelocity::ConstPtr&);
     #endif
     bool srv_callback(hear_msgs::set_float::Request&, hear_msgs::set_float::Response&);
-    tf2::Matrix3x3 rot_offset, rot_offset_vision;
+    tf2::Matrix3x3 rot_offset, rot_offset_vision, rot_offset_PX4_ori;
     tf2::Vector3 trans_offset, trans_offset_vision;
 
     tf2::Vector3 opti_pos, prev_pos, opti_vel, prev_diff, _hold;
@@ -65,10 +72,12 @@ public:
     ~ROSUnit_PoseProvider(){}
     std::vector<ExternalOutputPort<Vector3D<float>>*> registerOptiPose(std::string t_name);
     std::vector<ExternalOutputPort<Vector3D<float>>*> registerVisionPose(std::string t_name);
-    std::vector<ExternalOutputPort<Vector3D<float>>*> registerPX4Pose(std::string t_name);
+    ExternalOutputPort<Vector3D<float>>* registerPX4Pose(std::string t_name);
+    ExternalOutputPort<Vector3D<float>>* registerPX4Vel(std::string t_name);
+    ExternalOutputPort<Vector3D<float>>* registerPX4ImuOri(std::string t_name);
+    ExternalOutputPort<Vector3D<float>>* registerPX4ImuAngularRate(std::string t_name);
     ExternalOutputPort<Vector3D<float>>* registerImuOri(std::string t_name);
     ExternalOutputPort<Vector3D<float>>* registerImuAngularRate(std::string t_name);
-    ExternalOutputPort<Vector3D<float>>* registerPX4ImuAngularRate(std::string t_name);
     ExternalOutputPort<Vector3D<float>>* registerImuAcceleration(std::string t_name);
 };
 
--- src/ROSUnit_PoseProvider.cpp
+++ src/ROSUnit_PoseProvider.cpp
@@ -35,20 +35,45 @@ std::vector<ExternalOutputPort<Vector3D<float>>*> ROSUnit_PoseProvider::register
     return std::vector<ExternalOutputPort<Vector3D<float>>*>{vision_pos_port, vision_vel_port, vision_ori_port};
 }
 
-std::vector<ExternalOutputPort<Vector3D<float>>*> ROSUnit_PoseProvider::registerPX4Pose(std::string t_name){
+#ifdef PX4
+ExternalOutputPort<Vector3D<float>>* ROSUnit_PoseProvider::registerPX4Pose(std::string t_name){
     m_server = nh_.advertiseService("set_height_offset", &ROSUnit_PoseProvider::srv_callback, this);
     rot_offset.setRPY(0.0, 0.0, 0.0);
     trans_offset.setZero();
     
-    opti_pos_port = new ExternalOutputPort<Vector3D<float>>();
-    opti_pos_port->write(Vector3D<float>(0,0,0));
-    opti_vel_port = new ExternalOutputPort<Vector3D<float>>();
-    opti_vel_port->write(Vector3D<float>(0,0,0));
-    opti_ori_port = new ExternalOutputPort<Vector3D<float>>();
-    opti_ori_port->write(Vector3D<float>(0,0,0));
-    opti_sub = nh_.subscribe(t_name, 10, &ROSUnit_PoseProvider::callback_px4_pose, this, ros::TransportHints().tcpNoDelay());
-    return std::vector<ExternalOutputPort<Vector3D<float>>*>{opti_pos_port, opti_vel_port, opti_ori_port};
+    px4_pos_port = new ExternalOutputPort<Vector3D<float>>();
+    px4_pos_port->write(Vector3D<float>(0,0,0));
+    // px4_vel_port = new ExternalOutputPort<Vector3D<float>>();
+    // px4_vel_port->write(Vector3D<float>(0,0,0));
+    // px4_ori_port = new ExternalOutputPort<Vector3D<float>>();
+    // px4_ori_port->write(Vector3D<float>(0,0,0));
+    px4_pos_sub = nh_.subscribe(t_name, 10, &ROSUnit_PoseProvider::callback_px4_pose, this, ros::TransportHints().tcpNoDelay());
+    return px4_pos_port;
 }
+
+ExternalOutputPort<Vector3D<float>>* ROSUnit_PoseProvider::registerPX4Vel(std::string t_name){
+    px4_vel_port = new ExternalOutputPort<Vector3D<float>>();
+    px4_vel_port->write(Vector3D<float>(0,0,0));
+
+    px4_vel_sub = nh_.subscribe(t_name, 10, &ROSUnit_PoseProvider::callback_px4_vel, this, ros::TransportHints().tcpNoDelay());
+    return px4_vel_port;
+}
+
+ExternalOutputPort<Vector3D<float>>* ROSUnit_PoseProvider::registerPX4ImuOri(std::string t_name){
+    rot_offset_PX4_ori.setRPY(M_PI, 0.0, 0.0); 
+
+    px4_ori_port = new ExternalOutputPort<Vector3D<float>>();
+    px4_ori_port->write(Vector3D<float>(0,0,0));
+    px4_ori_sub = nh_.subscribe(t_name, 10, &ROSUnit_PoseProvider::callback_px4_ori, this, ros::TransportHints().tcpNoDelay());
+    return px4_ori_port;
+}
+ExternalOutputPort<Vector3D<float>>* ROSUnit_PoseProvider::registerPX4ImuAngularRate(std::string t_name){
+    px4_imu_angular_rt_port = new ExternalOutputPort<Vector3D<float>>();
+    px4_imu_angular_rt_port->write(Vector3D<float>(0,0,0));
+    px4_ang_vel_sub = nh_.subscribe(t_name, 10, &ROSUnit_PoseProvider::callback_px4_angular_vel, this, ros::TransportHints().tcpNoDelay());
+    return px4_imu_angular_rt_port;
+}
+#endif
 
 ExternalOutputPort<Vector3D<float>>* ROSUnit_PoseProvider::registerImuOri(std::string t_name){
     imu_ori_port = new ExternalOutputPort<Vector3D<float>>();
@@ -63,14 +88,6 @@ ExternalOutputPort<Vector3D<float>>* ROSUnit_PoseProvider::registerImuAngularRat
     xsens_ang_vel_sub = nh_.subscribe(t_name, 10, &ROSUnit_PoseProvider::callback_angular_vel, this, ros::TransportHints().tcpNoDelay());
     return imu_angular_rt_port;
 }
-#ifdef PX4
-ExternalOutputPort<Vector3D<float>>* ROSUnit_PoseProvider::registerPX4ImuAngularRate(std::string t_name){
-    px4_imu_angular_rt_port = new ExternalOutputPort<Vector3D<float>>();
-    px4_imu_angular_rt_port->write(Vector3D<float>(0,0,0));
-    px4_ang_vel_sub = nh_.subscribe(t_name, 10, &ROSUnit_PoseProvider::callback_px4_angular_vel, this, ros::TransportHints().tcpNoDelay());
-    return px4_imu_angular_rt_port;
-}
-#endif
 ExternalOutputPort<Vector3D<float>>* ROSUnit_PoseProvider::registerImuAcceleration(std::string t_name){
     imu_acc_port = new ExternalOutputPort<Vector3D<float>>();
     imu_acc_port->write(Vector3D<float>(0,0,0));
@@ -183,55 +200,7 @@ void ROSUnit_PoseProvider::callback_vision_pose(const geometry_msgs::PoseWithCov
     vision_ori_port->write(vec_ori);
 }
 
-void ROSUnit_PoseProvider::callback_px4_pose(const geometry_msgs::PoseStamped::ConstPtr& msg){
-    
-    tf2::Vector3 vel;
-    auto pos = tf2::Vector3({msg->pose.position.x, msg->pose.position.y, msg->pose.position.z});
-    auto calib_pos = pos; //rot_offset*pos - trans_offset;
 
-    Vector3D<float> vec = {(float)calib_pos.x(), (float)calib_pos.y(), (float)calib_pos.z()};
-
-    auto R_mat = tf2::Matrix3x3(tf2::Quaternion(msg->pose.orientation.x, msg->pose.orientation.y, msg->pose.orientation.z, msg->pose.orientation.w ));
-
-    // R_mat = rot_offset * R_mat * rot_offset.transpose();
-    tf2Scalar yaw, pitch, roll;
-    R_mat.getEulerYPR(yaw, pitch, roll);
-
-    Vector3D<float> vec_ori = {(float)roll, (float)pitch, (float)yaw};
-
-    // velocity calculation
-    if(first_read == 0){
-        first_read = 1;
-        prevT = msg->header.stamp;
-        prev_pos = pos;
-        vel = tf2::Vector3(0, 0, 0);
-        prev_diff = vel;
-    }else{
-        auto _dt = (msg->header.stamp - prevT).toSec();
-        auto diff = (pos - prev_pos)/_dt;
-        vel = diff;
-        if(first_read == 1){
-            first_read = 2;
-            prev_diff = diff;
-        }
-        auto d_diff = diff - prev_diff;
-        if(abs(d_diff.x()) > PEAK_THRESH || abs(d_diff.y()) > PEAK_THRESH || abs(d_diff.z()) > PEAK_THRESH){
-            vel = _hold;
-        }
-        else{
-            _hold = diff;
-        }
-        prev_diff = diff;
-        prev_pos = pos;
-        prevT = msg->header.stamp;
-    }
-    opti_vel = rot_offset*vel;
-    ////////////////////////
-
-    opti_pos_port->write(vec);
-    opti_vel_port->write(Vector3D<float>(opti_vel.x(), opti_vel.y(), opti_vel.z()));
-    opti_ori_port->write(vec_ori);
-}
 
 void ROSUnit_PoseProvider::callback_ori(const geometry_msgs::QuaternionStamped::ConstPtr& msg){
     
@@ -251,6 +220,47 @@ void ROSUnit_PoseProvider::callback_angular_vel(const geometry_msgs::Vector3Stam
 }
 
 #ifdef PX4
+void ROSUnit_PoseProvider::callback_px4_pose(const geometry_msgs::PoseStamped::ConstPtr& msg){
+    
+    auto pos = tf2::Vector3({msg->pose.position.x, msg->pose.position.y, msg->pose.position.z});
+    auto calib_pos = rot_offset*pos - trans_offset;
+
+    Vector3D<float> vec = {(float)calib_pos.x(), (float)calib_pos.y(), (float)calib_pos.z()};
+
+    auto R_mat = tf2::Matrix3x3(tf2::Quaternion(msg->pose.orientation.x, msg->pose.orientation.y, msg->pose.orientation.z, msg->pose.orientation.w ));
+
+    // R_mat = rot_offset * R_mat * rot_offset.transpose();
+    tf2Scalar yaw, pitch, roll;
+    R_mat.getEulerYPR(yaw, pitch, roll);
+
+    Vector3D<float> vec_ori = {(float)roll, (float)pitch, (float)yaw};
+
+
+    px4_pos_port->write(vec);
+}
+void ROSUnit_PoseProvider::callback_px4_vel(const geometry_msgs::TwistStamped::ConstPtr& msg){
+
+    auto vel = tf2::Vector3({msg->twist.linear.x, msg->twist.linear.y, msg->twist.linear.z});
+    auto calib_vel = rot_offset*vel - trans_offset;
+
+    Vector3D<float> vec = {(float)calib_vel.x(), (float)calib_vel.y(), (float)calib_vel.z()};
+
+
+    px4_vel_port->write(vec);
+}
+void ROSUnit_PoseProvider::callback_px4_ori(const mavros_msgs::VehicleAttitude::ConstPtr& msg){
+    
+    auto R_mat = tf2::Matrix3x3(tf2::Quaternion(msg->q_x, msg->q_y, msg->q_z, msg->q_w ));
+
+    R_mat = rot_offset_PX4_ori * R_mat * rot_offset_PX4_ori.transpose();
+    tf2Scalar yaw, pitch, roll;
+    R_mat.getEulerYPR(yaw, pitch, roll);
+
+    Vector3D<float> vec_ori = {(float)roll, (float)pitch, (float)yaw};
+
+
+    px4_ori_port->write(vec_ori);
+}
 void ROSUnit_PoseProvider::callback_px4_angular_vel(const mavros_msgs::VehicleAngularVelocity::ConstPtr& msg){
     Vector3D<float> vec = {(float)msg->angular_velocity_x, -(float)msg->angular_velocity_y, -(float)msg->angular_velocity_z};
 
